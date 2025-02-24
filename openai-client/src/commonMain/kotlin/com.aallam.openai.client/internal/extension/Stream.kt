285c7223ad3860e7e70245d5b5c852822b779795
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-02-25 00:20:58 +0800

    fix: space

--- openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/extension/Stream.kt
+++ openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/extension/Stream.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.isActive
 
 private const val STREAM_PREFIX = "data:"
-private const val STREAM_END_TOKEN = "$STREAM_PREFIX [DONE]"
+private const val STREAM_END_TOKEN = "[DONE]"
 
 /**
  * Get data as [Server-Sent Events](https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#Event_stream_format).
@@ -19,10 +19,11 @@ internal suspend inline fun <reified T> FlowCollector<T>.streamEventsFrom(respon
     try {
         while (currentCoroutineContext().isActive && !channel.isClosedForRead) {
             val line = channel.readUTF8Line() ?: continue
+            if (!line.startsWith(STREAM_PREFIX)) continue
+            val data = line.removePrefix(STREAM_PREFIX).trim()
             val value: T = when {
-                line.startsWith(STREAM_END_TOKEN) -> break
-                line.startsWith(STREAM_PREFIX) -> JsonLenient.decodeFromString(line.removePrefix(STREAM_PREFIX))
-                else -> continue
+                data.startsWith(STREAM_END_TOKEN) -> break
+                else -> JsonLenient.decodeFromString(line.removePrefix(STREAM_PREFIX))
             }
             emit(value)
         }
